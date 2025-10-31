569d30ca7174b4853cf1f3a4a9e302270915f87e
MatvejAver <avermatvej@icloud.com>
2025-10-31 17:09:08 +0500

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,74 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.hp = randint(100,140)
+        self.power = randint(10,20)
+
+
+        Pokemon.pokemons[pokemon_trainer] = self
+    # Метод для получения картинки покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+    
+    # Метод для получения имени покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['home']['front_default'])
+        else:
+            return "Pikachu"
+
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покеомона: {self.name}, сила твоего покеомона: {self.power}, зворовье твоего покеомона: {self.hp}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+    
+    def wizard_chets(self):
+        self.chance = randint(1,2)
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            self.chance = randint(1,5)
+            if self.chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
+
+class Wizard(Pokemon):
+    pass
+
+class Fighter(Pokemon):
+
+    def attack(self, enemy):
+        self.super_power = randint(5,15)
+        self.power += self.super_power
+        res = super().attack(enemy)
+        self.power -= self.super_power
+        return res + f"\nБоец применил супер-атаку силой:{self.super_power}"
